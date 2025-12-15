d0f54416dc591c0d33d0c6decd18851c37628c7b
Fernando del Olmo <fernandodelolmovelayos@gmail.com>
2025-12-15 10:35:43 +0100

    fixing faro

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,32 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { matchRoutes } from 'react-router-dom';
+import { initializeFaro, createReactRouterV6DataOptions, ReactIntegration, getWebInstrumentations, } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+    url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/aee5b2e987051a8732837c3452ee9015',
+    app: {
+        name: 'GuideHero',
+        version: '1.0.0',
+        environment: 'production'
+    },
+
+    instrumentations: [
+        // Mandatory, omits default instrumentations otherwise.
+        ...getWebInstrumentations(),
+
+        // Tracing package to get end-to-end visibility for HTTP requests.
+        new TracingInstrumentation(),
+
+        // React integration for React applications.
+        new ReactIntegration({
+            router: createReactRouterV6DataOptions({
+                matchRoutes,
+            }),
+        }),
+    ],
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
